f73d606c9a32dcbf59a7b5738af11b203c1e6b84
Brayan Mora <brayan_mora@outlook.com>
2024-05-11 09:12:39 -0500

    refactor: Update name variable in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { getInitials } from "./helpers";
 
 function App() {
-  const name = "Juan";
+  const name = "John Doe";
   const initials = getInitials(name);
 
   return (
-    <div className="bg-black h-screen w-screen flex gap-4 flex-col">
+    <div className="bg-black h-screen w-screen flex gap-4 flex-col p-4">
       <span className="text-white text-xl capitalize">Hello {name}!</span>
-      <div>
+      <div className="w-16 h-16 flex">
         <span
-          className="text-black p-4 bg-white rounded-full"
+          className="text-black p-4 bg-white rounded-full flex flex-1 items-center justify-center text-lg font-bold"
           data-testid="initials"
         >
           {initials}
